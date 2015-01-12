b48335bd8e9cca2692041d5058606596fd800538
k2works <kakimomokuri@gmail.com>
2015-01-12 18:01:53 +0900

    セットアップ

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+var
+LIVE_RELOAD_PORT = 35729,
+lrSnippet = require('connect-livereload')({port: LIVE_RELOAD_PORT}),
+mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    dirs: {
+      src: 'js',
+      dest: 'js'
+    },
+    jshint: {
+      beforeconcat: ['<%= dirs.src %>/*.js'],
+      afterconcat: ['<%= dirs.dest %>/*.js']
+    },
+    concat: {
+      options: {
+        banner: '/*! some copyright information here */'
+      },
+      js: {
+        src: ['<%= dirs.src %>/*.js'],
+        dest: '<%= dirs.dest %>/<%= pkg.name %>.js'
+      }      
+    },
+    uglify: {
+      options: {
+        banner: '/*! some copyright information here */',
+        sourceMap: true
+      },
+      dest: {
+        files: {
+          '<%= dirs.dest %>/<%= pkg.name %>.min.js':
+          '<%= dirs.dest %>/<%= pkg.name %>.js'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: LIVE_RELOAD_PORT
+      },
+      files: [
+        '<%= dirs.src %>/*.js',
+        './*.html'
+      ],
+      tasks: ['build']
+    },
+    connect: {
+      options: {
+        port: 9000,
+        hostname: '0.0.0.0'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, './')
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        url: 'http://localhost:<%=connect.options.port%>'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+
+  grunt.registerTask('build', 'Build JavaScript Files', [
+    'jshint:beforeconcat',
+    'concat',
+    'jshint:afterconcat',
+    'uglify'
+  ]);
+
+  grunt.registerTask('default', ['connect:livereload', 'open', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,37 @@
+Backbone.jsで学ぶMVCフレームワーク入門(実践編)
+===
+# 目的
+# 前提
+| ソフトウェア     | バージョン    | 備考         |
+|:---------------|:-------------|:------------|
+| OS X           |10.8.5        |             |
+| Backbone.js  　|1.1.2         |             |
+| Underscore.js  |1.7.0         |             |
+| jQuery         |1.11.2         |             |
+| Backbone localStorage    　　　| 1.1.15       |             |
+| bootstrap  　  |3.3.1          |             |
+
+# 構成
++ [セットアップ](#1)
++ [メモ帳アプリケーションの作成１](#2)
++ [メモ帳アプリケーションの作成２](#3)
++ [メモ帳アプリケーションの作成３](#4)
+
+# 詳細
+## <a name="1">セットアップ</a>
+```bash
+$ npm install
+$ grunt
+```
+## <a name="2">メモ帳アプリケーションの作成１</a>
+## <a name="3">メモ帳アプリケーションの作成２</a>
+## <a name="4">メモ帳アプリケーションの作成３</a>
+
+# 参照
++ [JavaScriptエンジニア養成読本](http://gihyo.jp/book/2014/978-4-7741-6797-8)
++ [JavaScriptエンジニア養成読本Backbone.js特集の訂正](http://text.ykhs.org/2014/10/22/javascript_engineer_training_book_fix.html)
++ [BACKBONE.JS](http://backbonejs.org/)
++ [UNDERSCORE.JS](http://underscorejs.org/)
++ [jQuery](http://jquery.com/)
++ [Backbone localStorage Adapter v1.1.15](https://github.com/jeromegn/Backbone.localStorage)
++ [Bootstrap](http://getbootstrap.com/)
